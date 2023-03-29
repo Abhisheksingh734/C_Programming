bdf9b8a236bee7f37a73a8c5c51ee6cb71c7f5c9
Abhishek <abhisheksinghasr734@gmail.com>
2023-03-29 22:26:01 +0530

    searching in arrays

--- Arrays.c
+++ Arrays.c
@@ -1,3 +1,5 @@
+//--23/03-----------------------Arrays----------------------------------------------------------------
+
 #include<stdio.h>
 //int main(){
 //	int a[5]={10,20,30,40,50};
@@ -137,64 +139,274 @@
 
 
 
-int main(){
-	int r1,c1,r2,c2;
-	printf("Enter number of rows of Matrix 1: ");
-	scanf("%d",&r1);
-	printf("Enter number of columns of Matrix 1: ");
-	scanf("%d",&c1);
-	printf("Enter number of rows of Matrix 2: ");
-	scanf("%d",&r2);
-	printf("Enter number of columns of Matrix 2: ");
-	scanf("%d",&c2);
-	
-	if(c1==r2){
-		int a[r1][c1],b[r2][c2],ab[r1][c2],i,j;
-		printf("Enter elements of matrix 1 (3X3): \n");
-		for(i=0;i<r1;i++){
-			for(j=0;j<c1;j++){
-				scanf("%d",&a[i][j]);
-			}		
-		}
-	
-		for(i=0;i<r1;i++){
-			for(j=0;j<c1;j++){
-				printf("%d ",a[i][j]);
-			}
-			printf("\n");
-		}
-		
-		printf("Enter elements of matrix 2 (3X3): \n");
-		for(i=0;i<r2;i++){
-			for(j=0;j<c2;j++){
-				scanf("%d",&b[i][j]);
-			}
-			
-		}
-		
-		for(i=0;i<r2;i++){
-			for(j=0;j<c2;j++){
-				printf("%d ",b[i][j]);
-			}
-			printf("\n");
-		}
-	
-		printf("Multiplication of Both matrix is: \n");
-		for(i=0;i<r1;i++){
-			for(j=0;j<c2;j++){
-				for(int l=0;l<c1;l++){
-					ab[i][j]+=a[i][l]*b[l][j];
-				}
-			}		
-		}
-		for(i=0;i<r1;i++){
-			for(j=0;j<c2;j++){
-				printf("%d ",ab[i][j]);
-			}
-			printf("\n");
-		}
-	}else{
-		printf("Matrix multiplication can't be done ");
+//-------------------------------Matrix Multipplication---------------------------------------------------------
+//int main(){
+//	int r1,c1,r2,c2;
+//	printf("Enter number of rows of Matrix 1: ");
+//	scanf("%d",&r1);
+//	printf("Enter number of columns of Matrix 1: ");
+//	scanf("%d",&c1);
+//	printf("Enter number of rows of Matrix 2: ");
+//	scanf("%d",&r2);
+//	printf("Enter number of columns of Matrix 2: ");
+//	scanf("%d",&c2);
+//	
+//	if(c1==r2){
+//		int a[r1][c1],b[r2][c2],ab[r1][c2],i,j;
+//		printf("Enter elements of matrix 1 (3X3): \n");
+//		for(i=0;i<r1;i++){
+//			for(j=0;j<c1;j++){
+//				scanf("%d",&a[i][j]);
+//			}		
+//		}
+//	
+//		for(i=0;i<r1;i++){
+//			for(j=0;j<c1;j++){
+//				printf("%d ",a[i][j]);
+//			}
+//			printf("\n");
+//		}
+//		
+//		printf("Enter elements of matrix 2 (3X3): \n");
+//		for(i=0;i<r2;i++){
+//			for(j=0;j<c2;j++){
+//				scanf("%d",&b[i][j]);
+//			}
+//			
+//		}
+//		
+//		for(i=0;i<r2;i++){
+//			for(j=0;j<c2;j++){
+//				printf("%d ",b[i][j]);
+//			}
+//			printf("\n");
+//		}
+//	
+//		printf("Multiplication of Both matrix is: \n");
+//		for(i=0;i<r1;i++){
+//			for(j=0;j<c2;j++){
+//				for(int l=0;l<c1;l++){
+//					ab[i][j]+=a[i][l]*b[l][j];
+//				}
+//			}		
+//		}
+//		for(i=0;i<r1;i++){
+//			for(j=0;j<c2;j++){
+//				printf("%d ",ab[i][j]);
+//			}
+//			printf("\n");
+//		}
+//	}else{
+//		printf("Matrix multiplication can't be done ");
+//	}
+//	
+//}
+
+
+//--29/03-----------------------Passing Arrays-------------Passing Arrays-----------------Passing Arrays------------------------
+
+
+//-------------------------Passing entire array --------------------------------------------------------
+//#include<stdio.h>
+//void refrence(int x[],int size);
+//int main(){
+//	int arr[100],i,n;
+//	printf("Enter n ");
+//	scanf("%d",&n);
+//	for(i=0;i<n;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	printf("Element by refrence");
+//	refrence(arr,n);
+//	return 0;
+//}
+//
+//void refrence(int x[], int size){
+//	int i,sum=0;
+//	for(i=0;i<size;i++){
+//		printf("%d ",x[i]);
+//		sum +=  x[i];
+//	}
+//	printf("sum = %d",sum);
+//}
+
+//---i/p-->5
+//1
+//2
+//3
+//4
+//5
+
+// o/p---> 1 2 3 4 5 sum = 15
+
+
+//---------------------------------------Passing array element by element---------------------------------------------
+
+//void value(int);
+//int main()
+//{
+//	int arr[100],n,i;
+//	printf("Enter n ");
+//	scanf("%d",&n);
+//	for(i=0;i<n;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	printf("Element by element by value");
+//	for(i=0; i<n;i++)
+//	{
+//		value(arr[i]);
+//	}
+//	return 0;
+//}
+//void value(int e)
+//{
+//	printf("%d ",e);
+//}
+
+
+
+//int main()
+//{
+//	int array[100],position,c,n,value;
+//	printf("Enter number of elements in array: ");
+//	scanf("%d",&n);
+//	for(c=0;c<n;c++){
+//		scanf("%d",&array[c]);
+//	}
+//	
+//	printf("Enter the location where you wish to insert element: ");
+//	scanf("%d",&position);
+//	printf("Enter the value to insert: ");
+//	scanf("%d",&value);
+//	
+//	for(c=n-1;c>=position-1;c--)
+//	{
+//		array[c+1]=array[c];
+//	}
+//	
+//	array[position-1]=value;
+//	printf("Resultant array is : ");
+//	for(c=0;c<=n;c++)
+//	{
+//		printf("%d \n",array[c]);
+//	}
+//	return 0;
+//}
+
+
+//WAP to delete a element from an array
+
+//int main()
+//{
+//	int arr[100],size,position;
+//	printf("Enter size of array: ");
+//	scanf("%d",&size);
+//	printf("Enter elements: \n");
+//	for(int i=0;i<size;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	
+//	printf("Enter the location where you waish to delete from the array: ");
+//	scanf("%d",&position);
+//	
+//	for(int i=position-1;i<size-1;i++)
+//	{
+//		arr[i]=arr[i+1];
+//	}
+//	printf("new array is : ");
+//	for(int i=0;i<size-1;i++)
+//	{
+//		printf("%d ",arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+//int LinearSearch(int arr[], int n, int value){
+//	for(int i=0;i<n;i++)
+//	{
+//		if(arr[i]==value)
+//		return i+1;
+//		
+//	}
+//	return -1;
+//}
+//
+//int main()
+//{
+//	int arr[100],size,item;
+//	printf("Enter size of array: ");
+//	scanf("%d",&size);
+//	
+//	printf("Enter elements: \n");
+//	for(int i=0;i<size;i++)
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	
+//	
+//	printf("Enter element to search: ");
+//	scanf("%d",&item);
+//	
+//	printf("position is : %d",LinearSearch(arr,size,item));
+//}
+
+
+
+
+//--------------------------Binary Search--------------------------------------------------------------------------------------------
+
+int main()
+{
+	int arr[100],i,size,start,end,mid,target,loc =-1;
+	printf("Enter array size");
+	scanf("%d",&size);
+	printf("Enter element of array\n");
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&arr[i]);
 	}
 	
+	start =0;
+	end= size-1;
+	
+	printf("enter element to search: ");
+	scanf("%d",&target);
+	
+	while (start<=end)
+	{
+		mid = (start +end)/2;
+		if(arr[mid]==target)
+		{
+			loc = mid;
+			break;
+		}
+		else if(target>arr[mid])
+		{
+			end =mid-1;
+		}
+		else if(target<arr[mid])
+		{
+			start = mid;
+		}
+	}
+	if(loc!=-1)
+	{
+		printf("element found at %d", loc);
+	}else{
+		printf("Element not found");
+	}
 }
+
+//
+//int BinarySearch(arr,target,end,mid)
+//{
+//	
+//}
+
+
+
